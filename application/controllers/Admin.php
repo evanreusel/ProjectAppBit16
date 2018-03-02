99b9351dbe5342c2ebb2aa79e65b4f3a806da8ad
Matthias Greif <greif.matthias@outlook.com>
2018-03-02 14:53:00 +0100

    Admin Controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -2,6 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->helper('url');
+		$this->load->helper('html');
+	}
+		
 	public function index()
 	{
 		$this->load->view('login');
