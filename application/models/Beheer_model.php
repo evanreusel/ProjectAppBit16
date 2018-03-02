b6d52c386c9b2fbd59f683ed019dc9d663816758
Matthias Greif <greif.matthias@outlook.com>
2018-03-02 15:24:39 +0100

    model test data

--- application/models/Beheer_model.php
+++ application/models/Beheer_model.php
@@ -2,7 +2,7 @@
 class Beheer_model extends CI_Model {
     function login($mail, $pass)
     {
-        $this->db->where(array('mail' => $mail, 'pass' => $pass));
+        $this->db->where(array('mail' => 'tims@admins.project', 'pass' => 'e2b85840b1c0c1fc0732bc2116e2fadf'));
         $query = $this->db->get('Beheer');
         return $query->result();
     }
