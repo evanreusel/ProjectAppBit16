85056e5c3d93e587fc64369cb557a4e8abc95adf
Matthias Greif <greif.matthias@outlook.com>
2018-03-02 15:32:42 +0100

    Admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -13,14 +13,8 @@ class Admin extends CI_Controller {
 	}
 
 	public function login($username, $pass){
-		// $username = $this->input->post('username');
-		// $pass = $this->input->post('pass');
-		$data['return'] = '';
-
-		if($username != '' && $pass != ''){
-			$this->load->model('beheer_model');
-			$data['return'] = json_encode($this->beheer_model->login($username, $pass));
-		}
+		$this->load->model('beheer_model');
+		$data['return'] = json_encode($this->beheer_model->login($username, $pass));
 		
 		$this->load->view('req_output', $data);
 	}
