392aaf4e7d8af283d1068b1eb37d1d788c8db13a
Erik <evanreusel@gmail.com>
2018-03-09 00:05:24 +0100

    Path Fixes Main Template

--- application/views/main.php
+++ application/views/main.php
@@ -5,8 +5,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Login</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" media="screen" href="../assets/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" media="screen" href="../assets/css/mdb.min.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="assets/css/mdb.min.css" />
 
 	<script src="main.js"></script>
 </head>
@@ -67,7 +67,7 @@
 		<h1>Hoi</h1>
 
 	</div>
-<script src="../assets/js/bootstrap.js"/>
-<script src="../assets/js/mdb.min.js"/>
+<script src="assets/js/bootstrap.js"/>
+<script src="assets/js/mdb.min.js"/>
 </body>
 </html>
