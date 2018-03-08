072a85e8f0c93be23e1cdfcea7bbf464b9f3737b
Erik <evanreusel@gmail.com>
2018-03-08 21:48:54 +0100

    update main

---
+++ application/views/main.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Personeelsfeest</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="assets/css/mdb.css" />
+    <link rel="stylesheet" href="assets/css/fontawesome.css" />
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    
+</body>
+</html>
