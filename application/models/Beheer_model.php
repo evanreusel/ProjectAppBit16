d21c1c01381aef72b53d77510114d20075f63258
greif.matthias <greif.matthias@outlook.com>
2018-03-09 14:20:49 +0100

    get pw hash default

--- application/models/Beheer_model.php
+++ application/models/Beheer_model.php
@@ -5,6 +5,8 @@ class Beheer_model extends CI_Model {
         $this->db->where(array('username' => $username));
         $query = $this->db->get('Beheer');
 
+        print_r(password_hash('gmatthias', PASSWORD_DEFAULT));
+
         if (password_verify($query->row()->pass, $pass)) {
             return $query->result();
         }
