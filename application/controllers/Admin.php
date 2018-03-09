cd510aef1a270d54d63de6e167a697890776589f
greif.matthias <greif.matthias@outlook.com>
2018-03-09 14:56:06 +0100

    Commenting: Admin Controller

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,19 +6,25 @@ class Admin extends CI_Controller {
 	{
 		parent::__construct();
 
+		// Autoload
 		$this->load->library('session');
 
+		// Check location is home of admin controller
 		$is_home = ($this->router->class === 'admin' && $this->router->method === 'index') ? true : false;
 
+		// Homepage check
 		if(!$is_home){
+			// Redirect to login if no session started
 			if(!$this->session->has_userdata('id')){
 				redirect('/admin/index', 'location');
 			}
 		}
 	}
-		
+	
+	// DEFAULT
 	public function index()
 	{
+		// Load default login view
 		$data['message'] = "Welcome admin | Login";
 	    $data['view'] = 'login';
 		$data['css_files'] = array("login.css");
@@ -26,19 +32,27 @@ class Admin extends CI_Controller {
 		$this->load->view('template/main', $data);
 	}
 
+	// API
+	// LOGIN
 	public function login($username = null, $pass = null){
 		$data['return'] = '';
 		
+		// Check login vals
 		if(!is_null($username) && !is_null($pass)){
+			// Get data from db
 			$this->load->model('beheer_model');
 			$login_return = $this->beheer_model->login($username, $pass);
 
+			// Set session vars if succeeded
 			if($login_return != ''){
 				$this->session->set_userdata('id', $login_return->id);
 			}
+
+			// Return data
 			$data['return'] = json_encode($login_return);
 		}
 		
+		// Print in default api output view
 		$this->load->view('req_output', $data);
 	}
 }
