b3687910f73ee2b3f838577acac40707a9e73e52
greif.matthias <greif.matthias@outlook.com>
2018-03-09 22:00:54 +0100

    ADD: Docen/Student dashboards

--- application/controllers/Docent.php
+++ application/controllers/Docent.php
@@ -9,25 +9,19 @@ class Docent extends CI_Controller {
 		// Autoload
 		$this->load->library('session');
 
-		// Check location is home of admin controller
-		$is_home = ($this->router->class === 'docent' && $this->router->method === 'index') ? true : false;
-
-		// Homepage check
-		if(!$is_home){
-			// Redirect to home if no session started
-			if(!$this->session->has_userdata('id')){
-				redirect('/main/index', 'location');
-			}
+		// Redirect to mainscreen if no session started
+		if(!$this->session->has_userdata('id')){
+			redirect('/main/index', 'location');
 		}
 	}
 	
 	// DEFAULT
 	public function index()
 	{
-		// Load default login view
-		$data['message'] = "Welcome Docent";
+		// Load dash view
+		$data['message'] = "Welcome Student";
 	    $data['view'] = 'dash_docent';
-		$data['css_files'] = array("login.css");
+		$data['css_files'] = array("dash_docent.css");
 		$this->load->view('template/main', $data);
 	}
 }
--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -9,25 +9,19 @@ class Student extends CI_Controller {
 		// Autoload
 		$this->load->library('session');
 
-		// Check location is home of admin controller
-		$is_home = ($this->router->class === 'student' && $this->router->method === 'index') ? true : false;
-
-		// Homepage check
-		if(!$is_home){
-			// Redirect to home if no session started
-			if(!$this->session->has_userdata('id')){
-				redirect('/main/index', 'location');
-			}
+		// Redirect to mainscreen if no session started
+		if(!$this->session->has_userdata('id')){
+			redirect('/main/index', 'location');
 		}
 	}
 	
 	// DEFAULT
 	public function index()
 	{
-		// Load default login view
+		// Load dash view
 		$data['message'] = "Welcome Student";
 	    $data['view'] = 'dash_student';
-		$data['css_files'] = array("login.css");
+		$data['css_files'] = array("dash_student.css");
 		$this->load->view('template/main', $data);
 	}
 }
