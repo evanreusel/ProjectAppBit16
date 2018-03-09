b4dee4f39e5bc2ed7384b3dddc123b917398646c
greif.matthias <greif.matthias@outlook.com>
2018-03-09 22:17:10 +0100

    ADD: Main Controller homepage

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,22 @@
-
 <div class="container">
     <div class="row justify-content-start ">
-        <h1>
-            Hi there, welcome on the homepage of .. <br/>
-            the Thomas More International Days
-        </h1>
+        <div class="column">
+            <h1>
+                Hi there, welcome on the homepage of ..
+                <br/> the Thomas More International Days
+            </h1>
+            <a href="/index.php/admin/">
+                There you go admin
+            </a>
+            <p>
+                Samples by mail
+            </p>
+            <a href="/index.php/main/signin/1/0prol2vZH3IgYBMapBI2">
+                Sample student login
+            </a>
+            <a href="/index.php/main/signin/6/FR8QqonWJ56bSjokRRPH">
+                Sample docent login
+            </a>
+        </div>
     </div>
 </div>
