b509f29720ffad1171e3ad5fd806a28190b00a3d
greif.matthias <greif.matthias@outlook.com>
2018-03-09 13:59:47 +0100

    Login Model change

--- application/models/Beheer_model.php
+++ application/models/Beheer_model.php
@@ -2,9 +2,12 @@
 class Beheer_model extends CI_Model {
     function login($username, $pass)
     {
-        $this->db->where(array('username' => $username, 'pass' => $pass));
+        $this->db->where(array('username' => $username));
         $query = $this->db->get('Beheer');
-        return $query->result();
+
+        if (password_verify($query->row()->password, $hash)) {
+            return $query->result();
+        }
     }
 }
 ?>
