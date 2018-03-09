6b91d53488eb0ca121919cc6617b89814b4f70e6
greif.matthias <greif.matthias@outlook.com>
2018-03-09 14:01:19 +0100

    Login test

--- application/models/Beheer_model.php
+++ application/models/Beheer_model.php
@@ -5,6 +5,8 @@ class Beheer_model extends CI_Model {
         $this->db->where(array('username' => $username));
         $query = $this->db->get('Beheer');
 
+        print_r($query->row());
+
         if (password_verify($query->row()->password, $hash)) {
             return $query->result();
         }
