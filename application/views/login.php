9ab1531cbe18f6bbd32c83e7b0624e44eeddc924
greif.matthias <greif.matthias@outlook.com>
2018-03-09 13:16:35 +0100

    login view

--- application/views/login.php
+++ application/views/login.php
@@ -1,58 +1,56 @@
 <div class="row">
 	<div class="col"></div>
 	<div class="col-6">
-		<div class="modal fade" id="modalDefaultContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-			<!--Modal: Contact form-->
-			<div class="modal-dialog cascading-modal" role="document">
+		<!--Modal: Contact form-->
+		<div class="modal-dialog cascading-modal" role="document">
 
-				<!--Content-->
-				<div class="modal-content">
+			<!--Content-->
+			<div class="modal-content">
 
-					<!--Header-->
-					<div class="modal-header info-color white-text">
-						<h4 class="title">
-							<i class="fa fa-pencil"></i> Contact form</h4>
-						<button type="button" class="close waves-effect waves-light" data-dismiss="modal" aria-label="Close">
-							<span aria-hidden="true">×</span>
+				<!--Header-->
+				<div class="modal-header info-color white-text">
+					<h4 class="title">
+						<i class="fa fa-pencil"></i> Contact form</h4>
+					<button type="button" class="close waves-effect waves-light" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">×</span>
+					</button>
+				</div>
+				<!--Body-->
+				<div class="modal-body">
+
+					<!-- Default input name -->
+					<label for="defaultFormNameModalEx">Your name</label>
+					<input type="text" id="defaultFormNameModalEx" class="form-control form-control-sm">
+
+					<br>
+
+					<!-- Default input email -->
+					<label for="defaultFormEmailModalEx">Your email</label>
+					<input type="password" id="defaultFormEmailModalEx" class="form-control form-control-sm">
+
+					<br>
+
+					<!-- Default input subject -->
+					<label for="defaultFormSubjectModalEx">Subject</label>
+					<input type="text" id="defaultFormSubjectModalEx" class="form-control form-control-sm">
+
+					<br>
+
+					<!-- Default textarea message -->
+					<label for="defaultFormMessageModalEx">Your message</label>
+					<textarea type="text" id="defaultFormMessageModalEx" class="md-textarea form-control"></textarea>
+
+					<div class="text-center mt-4 mb-2">
+						<button class="btn btn-info">Send
+							<i class="fa fa-send ml-2"></i>
 						</button>
 					</div>
-					<!--Body-->
-					<div class="modal-body">
 
-						<!-- Default input name -->
-						<label for="defaultFormNameModalEx">Your name</label>
-						<input type="text" id="defaultFormNameModalEx" class="form-control form-control-sm">
-
-						<br>
-
-						<!-- Default input email -->
-						<label for="defaultFormEmailModalEx">Your email</label>
-						<input type="password" id="defaultFormEmailModalEx" class="form-control form-control-sm">
-
-						<br>
-
-						<!-- Default input subject -->
-						<label for="defaultFormSubjectModalEx">Subject</label>
-						<input type="text" id="defaultFormSubjectModalEx" class="form-control form-control-sm">
-
-						<br>
-
-						<!-- Default textarea message -->
-						<label for="defaultFormMessageModalEx">Your message</label>
-						<textarea type="text" id="defaultFormMessageModalEx" class="md-textarea form-control"></textarea>
-
-						<div class="text-center mt-4 mb-2">
-							<button class="btn btn-info">Send
-								<i class="fa fa-send ml-2"></i>
-							</button>
-						</div>
-
-					</div>
 				</div>
-				<!--/.Content-->
 			</div>
-			<!--/Modal: Contact form-->
+			<!--/.Content-->
 		</div>
+		<!--/Modal: Contact form-->
 	</div>
 	<div class="col"></div>
 </div>
