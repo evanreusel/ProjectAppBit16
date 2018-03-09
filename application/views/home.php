7625282944264f8f912d928db7ecce24aaae7bb3
Erik <evanreusel@gmail.com>
2018-03-09 02:17:25 +0100

    Update Home Page

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,49 @@
-<div class="container">
-    <div class="row justify-content-center vertical-center">
-        <div class="col-4">
-            <div class="card">
-                <div class="card-body">
-                    <h1>Welkom!</h1>
-                    <p>Thomas More is een unie van hogescholen binnen de Associatie KU Leuven. Jaarlijks wordt er een personeelsfeest georganiseerd voor de Thomas More campus in Geel.</p>
-                    <p>U kunt u hier registreren als gast of als vrijwilliger.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-4">
-            <div class="card">
-                <div class="card-body">
-                    <h1>Welkom!</h1>
-                    <p>Thomas More is een unie van hogescholen binnen de Associatie KU Leuven. Jaarlijks wordt er een personeelsfeest georganiseerd voor de Thomas More campus in Geel.</p>
-                    <p>U kunt u hier registreren als gast of als vrijwilliger.</p>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="container-fluid">
 
+    <!--Carousel Wrapper-->
+    <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+        <!--Indicators-->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+            <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+        </ol>
+        <!--/.Indicators-->
+        <!--Slides-->
+        <div class="carousel-inner" role="listbox">
+            <!--First slide-->
+            <div class="carousel-item active">
+                <img class="d-block" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" alt="First slide">
+            </div>
+            <!--/First slide-->
+            <!--Second slide-->
+            <div class="carousel-item">
+                <img class="d-block" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg" alt="Second slide">
+            </div>
+            <!--/Second slide-->
+            <!--Third slide-->
+            <div class="carousel-item">
+                <img class="d-block" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Third slide">
+            </div>
+            <!--/Third slide-->
+        </div>
+        <!--/.Slides-->
+        <!--Controls-->
+        <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        <!--/.Controls-->
+    </div>
+    <!--/.Carousel Wrapper-->
+
+</div>
+<div class="container">
+    <div class="row justify-content-start ">
+
+    </div>
 </div>
