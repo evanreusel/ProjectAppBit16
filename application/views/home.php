5048a0a937ea83c1b17f4027a980e4863702c685
Erik <evanreusel@gmail.com>
2018-03-09 01:48:35 +0100

    update Home Page

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,23 @@
-<div class="container-fluid">
-    <h1>Home Page <?php echo $message;?></h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h1>Welkom!</h1>
+                    <p>Thomas More is een unie van hogescholen binnen de Associatie KU Leuven. Jaarlijks wordt er een personeelsfeest georganiseerd voor de Thomas More campus in Geel.</p>
+                    <p>U kunt u hier registreren als gast of als vrijwilliger.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-body">
+                    <h1>Welkom!</h1>
+                    <p>Thomas More is een unie van hogescholen binnen de Associatie KU Leuven. Jaarlijks wordt er een personeelsfeest georganiseerd voor de Thomas More campus in Geel.</p>
+                    <p>U kunt u hier registreren als gast of als vrijwilliger.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
