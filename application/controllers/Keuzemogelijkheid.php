cacd866f39e644a1350bcbf00b7c265a8f665861
greifmatthias <greif.matthias@outlook.com>
2018-03-23 15:05:07 +0100

    CLEANUP

--- application/controllers/Keuzemogelijkheid.php
+++ application/controllers/Keuzemogelijkheid.php
@@ -7,25 +7,6 @@ class Keuzemogelijkheid extends CI_Controller{
     
      public function __construct() {
         parent::__construct();
-
-        $this->load->helper('url');
-        $this->load->helper('form');
-    }
-    
-    public function index() {
-        $this->load->model('Keuzemogelijkheid_model');
-            $data['activiteiten'] 
-                    = $this->Keuzemogelijkheid_model->getAllByNaamWithKeuzeOpties();
-            
-            $data['titel']  = 'Database beheer';
-        
-
-            $data['message'] = "Beheer";
-            $data['view'] = 'keuzemogelijkheid';
-            $data['css_files'] = array("dash_docent.css");
-            $this->load->view('template/main', $data);
-       
-        
     }
 
     public function get($id){
