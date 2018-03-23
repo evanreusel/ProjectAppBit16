55c7cddfb224eed104806d992559d652050bcf56
greifmatthias <greif.matthias@outlook.com>
2018-03-23 14:48:31 +0100

    ADD: AdminBeheer Dash

--- application/views/dash_admin_adminbeheer.php
+++ application/views/dash_admin_adminbeheer.php
@@ -1,3 +1,38 @@
+<script> 
+
+function popup(data){    
+    $('#popup').dialog();    
+    console.log(data);    
+    splitdata = data.split(',');
+    var id = splitdata[0];
+    var user = splitdata[1];
+    $('#popupnaam').text(user);
+    $('#popupja').val(id);
+}
+
+ $(document).ready(function () {
+    $('#popup').hide();
+    $('.verwijder').click(function(){
+        var data = $(this).val();
+        console.log('ok');
+        popup(data);
+    })
+
+    $('#popupnee').click(function(){
+        $('#popup').dialog("close");
+    });
+
+    $('#popupja').click(function(){
+        $()
+    });
+    });
+</script>
+
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+
 <table>
     <tr>
         <td>User</td>
@@ -10,11 +45,22 @@
         foreach($data['admins'] as $admin){
             echo "<tr> \n
             <td>" . $admin->username . "</td> \n
-            <td> ***** </td> \n
+            <td> *** </td> \n
             <td>" . anchor("admin/dash/updateadmin/$admin->id","<button>Aanpassen</button>") . "</td>
-            <td>" . anchor("admin/delete/$admin->id","<button>Verwijderen</button>") . "</td>
+            <td>" . form_button("verwijder","verwijderen",array('value'=>"$admin->id,$admin->username",'class'=>"verwijder")) . "</td>
             </tr> \n";
         }
-        echo '<tr><td rowspan="4">' . anchor("admin/dash/updateadmin","<button>Niewe admin</button>") . '</td></tr>';
+        echo '<tr><td rowspan="4">' . anchor("admin/dash/updateadmin","<button>Niewe admin</button>") . '</td></tr>
+        <td>' . anchor("admin","home") . "</td>";
     ?>
 </table>
+
+<div id="popup" title="Waarschuwing">
+<?php $attributes = array('name' => 'verwijderform', 'id' => 'verwijderform', 'role' => 'form');
+echo form_open("admin/delete")?>
+    Bent u zeker dat u <div id="popupnaam"></div> wil verwijderen?
+    <br/>
+    <button id="popupja" name="id">Ja</button>
+    <button id="popupnee">Nee</button>
+    <?php echo form_close(); ?>
+</div>
