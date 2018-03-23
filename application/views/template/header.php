f05bbdb3739c0cd82a483734a5593d7b6929bdae
greifmatthias <greif.matthias@outlook.com>
2018-03-23 14:05:05 +0100

    FIX: Navbar Currentuser

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -51,7 +51,7 @@
             <?php if(count($nav_specs['actions']) > 0) { ?>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <?php echo $user->name; ?>
+                        <?php echo $user->username; ?>
                     </a>
                     
                     <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
