23e17e8330974e1711d6ebbdcf43ae0310f2b9fd
greifmatthias <greif.matthias@outlook.com>
2018-03-23 15:36:32 +0100

    DEBUG: JaargangBeheer View

--- application/views/dash_admin_jaargangbeheer.php
+++ application/views/dash_admin_jaargangbeheer.php
@@ -11,22 +11,22 @@
     <?php foreach($data['jaargangen'] as $jaargang){ ?>
     <tr>
         <td>
-            <?php echo $jaargang['naam']; ?>
+            <?php echo $jaargang->naam; ?>
         </td>
         <td>
-            <?php echo $jaargang['info']; ?>
+            <?php echo $jaargang->info; ?>
         </td>
         <td>
-            <?php echo $jaargang['thema']; ?>
+            <?php echo $jaargang->thema; ?>
         </td>
         <td>
-            <?php echo $jaargang['beginTijdstip']; ?>
+            <?php echo $jaargang->beginTijdstip; ?>
         </td>
         <td>
-            <?php echo $jaargang['eindTijdstip']; ?>
+            <?php echo $jaargang->eindTijdstip; ?>
         </td>
         <td>
-            <?php echo $jaargang['actief']; ?>
+            <?php echo $jaargang->actief; ?>
         </td>
     </tr>
     <?php } ?>
