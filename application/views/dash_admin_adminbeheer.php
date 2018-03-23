cdf152ef6c02c63760a52c0073f5deff1c0990c7
greifmatthias <greif.matthias@outlook.com>
2018-03-23 09:59:08 +0100

    DEBUG: AdminBeheer functional changes

--- application/views/dash_admin_adminbeheer.php
+++ application/views/dash_admin_adminbeheer.php
@@ -11,11 +11,11 @@
             echo "<tr> \n
             <td>" . $admin->username . "</td> \n
             <td> ***** </td> \n
-            <td>" . anchor("admin/page_update/$admin->id","<button>Aanpassen</button>") . "</td>
+            <td>" . anchor("admin/dash/updateadmin/$admin->id","<button>Aanpassen</button>") . "</td>
             <td>" . anchor("admin/delete/$admin->id","<button>Verwijderen</button>") . "</td>
             </tr> \n";
         }
-        echo '<tr><td rowspan="4">' . anchor("admin/dash/addAdmin","<button>Niewe admin</button>") . '</td></tr>
+        echo '<tr><td rowspan="4">' . anchor("admin/dash/updateadmin","<button>Niewe admin</button>") . '</td></tr>
         <td>' . anchor("admin","home") . "</td>";
     ?>
 </table>
