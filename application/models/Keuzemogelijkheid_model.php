0e285f8bcc2d7e2ba6deddf1992290a11bef2b7a
greifmatthias <greif.matthias@outlook.com>
2018-03-23 14:34:35 +0100

    FIX: KeuzemogelijkheidModel load keuzeOptie

--- application/models/Keuzemogelijkheid_model.php
+++ application/models/Keuzemogelijkheid_model.php
@@ -29,7 +29,7 @@ class Keuzemogelijkheid_Model extends CI_Model {
         $query = $this->db->get('KeuzeMogelijkheid');
         $activiteiten = $query->result();
 
-        $this->load->model('keuzemogelijkheid_model');
+        $this->load->model('keuzeoptie_model');
 
         foreach ($activiteiten as $activiteit) {
             $activiteit->keuzeopties=
