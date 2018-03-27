e6c0cbc0d9aed274d8a8003af4c3e284654cd996
TimSwerts <de.swerts@hotmail.com>
2018-03-27 22:33:16 +0200

    datetimepickerr plugin

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -9,6 +9,7 @@
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>assets/css/bootstrap-datetimepicker.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>assets/css/mdb.min.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>assets/css/style.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url(); ?>assets/css/fontawesome-all.css" />
@@ -25,6 +26,7 @@
     <script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/mdb.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap-datetimepicker.js"></script>
 
     <!--Navbar-->
     <?php
