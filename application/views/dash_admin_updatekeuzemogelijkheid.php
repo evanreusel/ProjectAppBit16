900f52d90ba362b9f0dc581a73ff46d123b1fe32
TimSwerts <de.swerts@hotmail.com>
2018-03-27 22:41:01 +0200

    +

--- application/views/dash_admin_updatekeuzemogelijkheid.php
+++ application/views/dash_admin_updatekeuzemogelijkheid.php
@@ -1,6 +1,9 @@
 
-    <input size="16" type="text" value="2012-06-15 14:45" readonly class="form_datetime">
-     
+    <input name= "begin" size="16" type="text" value="" readonly class="form_datetime">
+    <label for="begin">Begin datum en tijdstip:</label>
+    <input name= "einde" size="16" type="text" value="" readonly class="form_datetime">
+    <label for="einde">Eind datum en tijdstip:</label>
+
     <script type="text/javascript">
         $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
     </script>            
