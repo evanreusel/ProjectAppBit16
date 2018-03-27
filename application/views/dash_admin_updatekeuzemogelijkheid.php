e08ebec76cb247fd9835536c0b4b37b6684cf604
TimSwerts <de.swerts@hotmail.com>
2018-03-27 22:46:53 +0200

    +

--- application/views/dash_admin_updatekeuzemogelijkheid.php
+++ application/views/dash_admin_updatekeuzemogelijkheid.php
@@ -1,8 +1,14 @@
 
-    <input name= "begin" size="16" type="text" value="" readonly class="form_datetime">
+    <label for="keuzemogelijkheid">Begin datum en tijdstip:</label>
+    <input id="keuzemogelijkheid" name="naam" type="text" value="">
+    
+
     <label for="begin">Begin datum en tijdstip:</label>
-    <input name= "einde" size="16" type="text" value="" readonly class="form_datetime">
+    <input id="begin" name="beginTijdstip" size="16" type="text" value="" readonly class="form_datetime">
     <label for="einde">Eind datum en tijdstip:</label>
+    <input id="einde" name="eindTijdstip" size="16" type="text" value="" readonly class="form_datetime">
+    <label for="deadline">Datum en tijdstip voor deadline:</label>
+    <input id="deadline" name="deadlineTijdstip" size="16" type="text" value="" readonly class="form_datetime">
 
     <script type="text/javascript">
         $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
