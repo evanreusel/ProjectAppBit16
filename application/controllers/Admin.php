0495609da657972787fe2319579470db2b0c7270
greifmatthias <greif.matthias@outlook.com>
2018-03-30 13:30:41 +0200

    FIX: DefaultView

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -125,7 +125,7 @@ class Admin extends CI_Controller {
 				$this->load->model('plaats_model');
 				$data['plaatsen'] = $this->plaats_model->getAllByPlaatsnaam();
 			default:
-				$view = 'index';
+				$data['view'] = 'dash_admin_view';										// Default view
 			break;
 		}
 
