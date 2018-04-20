5fcff9f6624091308173d49f0ca9e620cec85d22
greifmatthias <greif.matthias@outlook.com>
2018-04-20 13:37:51 +0200

    Quick additions

--- application/views/dash_admin_jaargangbeheer.php
+++ application/views/dash_admin_jaargangbeheer.php
@@ -1,3 +1,6 @@
 <?php
     echo $data['jaargang']->naam;
 ?>
+
+<?php echo anchor("admin/dash/jaargangupdate/" . $data['jaargang']->id,'<i class="fa fa-edit"></i> Edit', array('class' => 'btn btn-primary')); ?>
+<?php echo anchor("admin/dash/keuzemogelijkheidbeheer/" . $data['jaargang']->id,'<i class="fa fa-folder"></i>', array('class' => 'btn btn-primary')); ?>
