b4a7bccef090874af8b7db0be0f55b58549693c5
unknown <daan proost>
2018-04-20 15:39:44 +0200

    jyf

--- application/views/dash_admin_plaatsToevoegen.php
+++ application/views/dash_admin_plaatsToevoegen.php
@@ -72,7 +72,7 @@ if(isset($huidigePlaats)){$plaatsTest = $huidigePlaats->naam; $locatieTest = $hu
         'class' => 'form-control',
         'required' => 'required'));
 
-        echo form_hidden('id'=>"id", 'value' => 0, 'name'=>'id');
+        echo form_hidden(array('id'=>'id', 'value' => 0, 'name'=>'id'));
 
     echo form_submit('knop', 'Verzenden');
     echo form_close();
