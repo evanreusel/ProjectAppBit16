38c1d867e35c530cbd09ebe580f46551a37b450f
TimSwerts <de.swerts@hotmail.com>
2018-04-23 12:57:38 +0200

    nieuwe view voor keuzemogelijkheidToevoegen

---
+++ application/views/dash_admin_keuzemogelijkheidToevoegen.php
@@ -0,0 +1,53 @@
+<?php
+/* 
+TIM SWERTS
+LAST UPDATED: 18 03 30
+DASH ADMIN KEUZEMOGELIJKHEID ADD/UPDATE
+*/
+
+    $arrayparameters = array();
+    $arrayparameters['id'] = 'send';
+    $arrayparameters['value'] = '0';
+    $arrayparameters['type'] = 'submit';
+    $arrayparameters['content'] = "Bevestig";
+    
+    $jaren = array('');
+    $toevoegen = array('');
+    $plaats = array('');
+
+    foreach ($plaatsen as $plek) {
+        array_push($plaats, $plek->naam);
+    }
+?>
+    
+
+
+    <?php echo form_open('keuzemogelijkheid/update', array('name' => 'keuzemogelijkheidFrom', 'id' => 'keuzemogelijkheidForm', 'role' => 'form'));  ?>
+    
+    <label for="jaar"><?php echo $jaargang; ?></label>
+    <?php echo form_dropdown("jaar", $jaren) ?>
+    </br>
+    <label for="keuzemogelijkheid">Naam keuzemogelijkheid:</label>
+    <input id="keuzemogelijkheid" name="naam" type="text" value="">
+    </br>
+    <label for="plaats">Plaats:</label>
+    <?php echo form_dropdown("plaats", $plaats) ?>
+    </br>
+    <label for="begin">Begin datum en tijdstip:</label>
+    <input id="begin" name="beginTijdstip" size="16" type="text" value="" readonly class="form_datetime">
+    </br>
+    <label for="einde">Eind datum en tijdstip:</label>
+    <input id="einde" name="eindTijdstip" size="16" type="text" value="" readonly class="form_datetime">
+    </br>
+    <label for="deadline">Datum en tijdstip voor deadline:</label>
+    <input id="deadline" name="deadlineTijdstip" size="16" type="text" value="" readonly class="form_datetime">
+    </br>
+    <?php    
+        echo form_button($arrayparameters);
+        echo form_close();
+    ?>
+
+    
+    <script type="text/javascript">
+        $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
+    </script>            
