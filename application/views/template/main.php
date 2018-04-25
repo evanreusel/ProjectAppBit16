c330a86b67c4ac47cbe1c1cbc0f4811b48a1dec4
greifmatthias <greif.matthias@outlook.com>
2018-04-25 15:10:41 +0200

    fix

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -30,7 +30,7 @@
 
 <body>
     <script src="<?php echo base_url(); ?>assets/js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="https://unpkg.com/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+    <script src="https://unpkg.com/popper.js@1.14.3/dist/umd/popper.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/mdb.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/bootstrap-datetimepicker.js"></script>
