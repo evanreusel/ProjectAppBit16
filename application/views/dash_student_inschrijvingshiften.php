98a9255f4972367dabb1d66b91495100fe89cda5
TimSwerts <de.swerts@hotmail.com>
2018-04-25 18:17:38 +0200

    +

--- application/views/dash_student_inschrijvingshiften.php
+++ application/views/dash_student_inschrijvingshiften.php
@@ -2,7 +2,7 @@
 
     echo '<div class="shiften"><h2>'.$activiteit->naam.'</h2><ul class="list-group>';
     foreach ($activiteit->taken as $taak) {
-        echo '<li class="list-group-item d-flex justify-content-between align-items-center list-group-item-primary"><p><b>'.$taak->functie.':</b></p><ul class="list-group">';
+        echo '<li class="list-group-item d-flex justify-content-between align-items-center list-group-item-primary">'.$taak->functie.':<ul class="list-group">';
         foreach ($taak->shiften as $shift ) {
             echo '<li class="list-group-item d-flex justify-content-between align-items-center">'.$shift->naam.'</li>';
         }
