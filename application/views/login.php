c030b7e8ef93ddcb7c00f079f1bb6ee621289a4c
greifmatthias <greif.matthias@outlook.com>
2018-04-25 14:13:13 +0200

    fix??

--- application/views/login.php
+++ application/views/login.php
@@ -47,7 +47,7 @@
 		$.get('<?php echo base_url(); ?>index.php/admin/login/' + $('#inpUsername').val() + '/' + $('#inpPass').val(), function (data) {
 			if(data != ''){
 				console.log(data);
-				// window.location.href = '<?php echo base_url(); ?>index.php/admin/dash/';
+				window.location.href = '<?php echo base_url(); ?>index.php/admin/dash/';
 			}
 		});
 	});
