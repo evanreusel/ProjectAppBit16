86ddf44f570ce6e4699b00cae066854e54eefb31
TimSwerts <de.swerts@hotmail.com>
2018-04-25 19:58:43 +0200

    +

--- application/views/dash_student_inschrijvingshiften.php
+++ application/views/dash_student_inschrijvingshiften.php
@@ -1,14 +1,14 @@
 <?php foreach($keuzemogelijkheden as $activiteit) {
 
-    echo '<div class="shiften"><ul class="list-group"><li class="list-group-item justify-content-between align-items-center list-group-item-primary"><h2>'.$activiteit->naam.'</h2><ul class="list-group">';
+    echo '<div class="shiften"><div class="panel-heading">'.$activiteit->naam.'</div><div class="panel-body"><ul class="list-group">';
     foreach ($activiteit->taken as $taak) {
         echo '<li class="list-group-item justify-content-between align-items-center"><p><b>'.$taak->functie.':</b></p><ul class="list-group">';
         foreach ($taak->shiften as $shift ) {
             echo '<li class="list-group-item justify-content-between align-items-center">'.$shift->naam;
-            echo '<button class="btn btn-success float-right" value="'.$shift->id.'">Inschrijven</button>';
+            echo '<button class="btn btn-success float-right inschrijven" value="'.$shift->id.'">Inschrijven</button>';
         }
         echo "</ul></li>";
     };
-    echo "</ul></li></ul></div>";
+    echo "</ul></li></div></div>";
 };
 ?>
