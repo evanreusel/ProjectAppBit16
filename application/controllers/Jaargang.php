34e49987386d7f428055442d6a45255bbcb65e76
greifmatthias <greif.matthias@outlook.com>
2018-04-25 14:40:57 +0200

    fix confirm

--- application/controllers/Jaargang.php
+++ application/controllers/Jaargang.php
@@ -73,7 +73,7 @@ class Jaargang extends CI_Controller{
         }
 
         // Redir
-        // redirect('admin/jaargangoverzicht');
+        redirect('admin/jaargangoverzicht');
     }
     
     // API
