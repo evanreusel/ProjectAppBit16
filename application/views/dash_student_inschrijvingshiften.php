176ea0d92af73e6da51f5b73e665c2ba6c7b1ed8
TimSwerts <de.swerts@hotmail.com>
2018-04-27 14:20:36 +0200

    +

--- application/views/dash_student_inschrijvingshiften.php
+++ application/views/dash_student_inschrijvingshiften.php
@@ -1,20 +1,15 @@
 <?php 
-$teller = 0;
 foreach($keuzemogelijkheden as $activiteit) {
     echo '<div class="shiften card"><div class="card-header bg-primary text-white">'.$activiteit->naam.'</div><div class="card-body"><ul class="list-group">';
     foreach ($activiteit->taken as $taak) {
         echo '<li class="list-group-item justify-content-between align-items-center"><p><b>'.$taak->functie.':</b></p><ul class="list-group">';
         foreach ($taak->shiften as $shift ) {
             
-                // if ($ingeschreven[$teller]->shiftId == $shift->id) {
-                //     echo '<li class="list-group-item justify-content-between align-items-center">'.$shift->naam;
-                //     echo '<button class="btn btn-warning float-right inschrijven" id="'.$shift->id.'">Uitschrijven</button>';
-                // }else {
+
                     echo '<li class="list-group-item justify-content-between align-items-center">'.$shift->naam;
+                    echo '<button class="btn btn-warning float-right inschrijven" id="'.$shift->id.'">Uitschrijven</button>';
                     echo '<button class="btn btn-primary float-right inschrijven" id="'.$shift->id.'">Inschrijven</button>';
-               
-            $teller++;
-        }
+        }     
         echo "</li></ul></li>";
     };
     echo "</ul></div></div>";
