ca5e76ad37e13a6948b556f4cf9bd80725bf63c7
TimSwerts <de.swerts@hotmail.com>
2018-04-27 09:39:21 +0200

    +

--- application/controllers/Student.php
+++ application/controllers/Student.php
@@ -80,4 +80,11 @@ class Student extends CI_Controller {
 
 		$this->load->view('template/main', $data);
 	}
+	
+	public function vrijwilligerInShiftToevoegen()
+    {
+        $data['return'] = "pamper";
+
+        $this->load->view('ajax_vrijwilligerinshift', $data);
+    }
 }
