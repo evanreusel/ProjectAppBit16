f2fcfb7fd7ffe6ce01a5abb0835a45e77665a892
greifmatthias <greif.matthias@outlook.com>
2018-04-27 15:36:55 +0200

    edit

--- application/views/dash_admin_jaargangbeheer.php
+++ application/views/dash_admin_jaargangbeheer.php
@@ -3,6 +3,43 @@
     LAST UPDATED: 18 04 27
     DASH ADMIN JAARGANGBEHEER
 -->
+<div class="items">
+    <?php
+        if($data['jaargang']->actief){
+    ?>
+    <div class="item">
+        <a href="/index.php/admin/dash/jaargangupdate/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
+            <i class="fa fa-edit"></i> Edit
+        </a>
+    </div>
+    <?php
+        }
+    ?>
+
+    <div class="item">
+        <a href="/index.php/admin/dash/keuzemogelijkheidbeheer/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
+            <i class="fa fa-folder"></i> Keuzemogelijkheden
+        </a>
+    </div>
+
+    <div class="item">
+        <a href="/index.php/admin/dash/vrijwilligersoverzicht/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
+            <i class="fa fa-edit"></i> Vrijwilligers
+        </a>
+    </div>
+
+    <div class="item">
+        <a href="/index.php/admin/dash/deelnemersoverzicht/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
+            <i class="fa fa-edit"></i> Deelnemers
+        </a>
+    </div>
+
+    <div class="item">
+        <a href="/index.php/admin/dash/personeelimporteren/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
+            <i class="fa fa-edit"></i> Importeren
+        </a>
+    </div>
+</div>
 
 <div id="dHeader" class="row justify-content-md-center">
     <div class="col-10">
@@ -23,40 +60,5 @@
                 echo $data['jaargang']->beginTijdstip . ' > ' . $data['jaargang']->eindTijdstip;
             ?>
         </p>
-    </div>
-</div>
-
-<div class="row justify-content-md-center">
-    <div class="items">
-        
-        <div class="item">
-            <a href="/index.php/admin/dash/jaargangupdate/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
-                <i class="fa fa-edit"></i> Edit
-            </a>
-        </div>
-
-        <div class="item">
-            <a href="/index.php/admin/dash/keuzemogelijkheidbeheer/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
-                <i class="fa fa-folder"></i> Keuzemogelijkheden
-            </a>
-        </div>
-
-        <div class="item">
-            <a href="/index.php/admin/dash/vrijwilligersoverzicht/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
-                <i class="fa fa-edit"></i> Vrijwilligers
-            </a>
-        </div>
-
-        <div class="item">
-            <a href="/index.php/admin/dash/deelnemersoverzicht/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
-                <i class="fa fa-edit"></i> Deelnemers
-            </a>
-        </div>
-
-        <div class="item">
-            <a href="/index.php/admin/dash/personeelimporteren/<?php $data['jaargang']->id; ?>" class="btn btn-primary">
-                <i class="fa fa-edit"></i> Importeren
-            </a>
-        </div>
     </div>
 </div>
