ab60ef74816a1cc6cf2386df7b1eff69c348e6f1
TimSwerts <de.swerts@hotmail.com>
2018-05-02 13:40:49 +0200

    +

--- application/views/ajax_vrijwilligersinshift.php
+++ application/views/ajax_vrijwilligersinshift.php
@@ -5,7 +5,7 @@
 </tr>
 <?php
 foreach ($shiften as $shift) {
-    echo $shit->persoonId;
+    echo $shift->persoonId;
     foreach ($shift->personen as $persoon) {
         echo "<tr><td>".$persoon->naam."</td><td>".$persoon->mail."</td></tr>";
     }
