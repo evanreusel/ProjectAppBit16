5609ddc0ab5c701d06ac7206ef393369eeab229d
Greif Matthias <greif.matthias@outlook.com>
2018-05-04 14:29:45 +0200

    layout fix

--- application/views/dash_admin_jaargangoverzicht.php
+++ application/views/dash_admin_jaargangoverzicht.php
@@ -6,7 +6,7 @@
 
 <p class="tooling">
     Hier kan u de verschillende edities voor uw evenement beheren. <br/>
-    <br>LET OP:</b> Er kan maar 1 editie tegelijkertijd actief zijn, eenmaal een editie afgesloten te hebben is er geen mogelijkheid meer deze terug te activeren.
+    <b>LET OP:</b> Er kan maar 1 editie tegelijkertijd actief zijn, eenmaal een editie afgesloten te hebben is er geen mogelijkheid meer deze terug te activeren.
 </p>
 
 <table class="table">
