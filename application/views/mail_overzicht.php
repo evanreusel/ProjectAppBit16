89cc8db8f7a85be227653c43e1dc7c53dbe12352
Erik <evanreusel@gmail.com>
2018-05-08 23:42:18 +0200

    Mail overzicht

--- application/views/mail_overzicht.php
+++ application/views/mail_overzicht.php
@@ -23,10 +23,9 @@
                                 <p><?php echo $reminder->status ?></p>
                             </div>
                             <div class="card-footer">
-
                                 <div class="btn-group align-content-center btn-block">
-                                    <a href="#" class="btn btn-success btn-block">Aanpassen</a>
-                                    <a href="#" class="btn btn-danger  btn-block">Verwijderen</a>
+                                    <a href="#modalReminder" class="btn btn-success btn-block" data-toggle="modal" data-target="#modalReminder">Aanpassen</a>
+                                    <button class="btn btn-danger  btn-block">Verwijderen</button>
                                 </div>
                             </div>
                         </div>
@@ -76,23 +75,14 @@
     </div>
 
 </div>
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ontvangers</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body" id="ontvangers_content">
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
+<div class="modal hide fade" id="modalReminder">
+    <div class="modal-header">
+        <button class="close" data-dismiss="modal">×</button>
+        <h3>Modal header</h3>
+    </div>
+    <div class="modal-body">
+        <p>some content</p>
+        <input type="text" name="bookId" id="bookId" value=""/>
     </div>
 </div>
 <script>
