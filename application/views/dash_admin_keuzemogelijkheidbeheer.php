4708d1d9619650f01145e240d01d8fe6d44cf8f9
TimSwerts <de.swerts@hotmail.com>
2018-05-09 10:16:07 +0200

    modal voor verwijderen

--- application/views/dash_admin_keuzemogelijkheidbeheer.php
+++ application/views/dash_admin_keuzemogelijkheidbeheer.php
@@ -36,16 +36,16 @@ if(count($data['keuzemogelijkheden']) > 0) {
                 }
 
                 echo '<td>'.anchor('admin/dash/updatekeuzeoptie/'.$keuzeoptie->id.'u','<button class="btn btn-round btn-warning" title="Druk hier om deze keuzeoptie aan te passen"><i class="fa fa-cog"></i> Aanpassen</button>').'</td>';
-                echo '<td>'.anchor('keuzeoptie/delete/'.$keuzeoptie->id,'<button class="btn btn-round btn-danger" title="Druk hier om deze keuzeoptie te verwijderen"><i class="fa fa-trash"></i> Verwijderen</button>').'</td>';
-
+                // echo '<td>'.anchor('keuzeoptie/delete/'.$keuzeoptie->id,'<button class="btn btn-round btn-danger" title="Druk hier om deze keuzeoptie te verwijderen"><i class="fa fa-trash"></i> Verwijderen</button>').'</td>';
+                echo '<td><button class="deleteKeuzeptie btn btn-round btn-warning" title="Druk hier om deze keuzeoptie aan te passen" data-toggle="modal" data-target="#keuzeoptieModal" value="'.$keuzeoptie->id.'"><i class="fa fa-cog"></i> Aanpassen</button></td>';
                    echo "</tr>"; 
             }
 
             echo "</table>";
             echo anchor('admin/dash/updatekeuzeoptie/'.$activiteit->id.'i',"Keuzeoptie toevoegen",'class="btn btn-primary" title="Druk hier om een keuzeoptie toe te voegen"');
-            echo anchor('admin/dash/takenbeheer/'.$activiteit->id,'<button class="btn btn-round btn-warning" title="Druk hier om taken te beheren bij deze keuzemogelijkheid"><i class="fa fa-cog"></i>' .$activiteit->naam. ' vrijwilliger taken</button>').'</td>';
-            echo anchor('admin/dash/updatekeuzemogelijkheid/'.$activiteit->id,'<button class="btn btn-round btn-warning" title="Druk hier om de keuzemogelijkheid aan te passen"><i class="fa fa-cog"></i>' .$activiteit->naam. ' aanpassen</button>').'</td>';
-            echo anchor('Keuzemogelijkheid/delete/' . $activiteit->id, '<button type="button" class="btn btn-danger btn-round" title="Druk hier om de keuzemogelijkheid te verwijderen"><i class="fa fa-trash"></i>'. $activiteit->naam . 'Verwijderen</button></div>');
+            echo anchor('admin/dash/takenbeheer/'.$activiteit->id,'<button class="btn btn-round btn-warning" title="Druk hier om taken te beheren bij deze keuzemogelijkheid"><i class="fa fa-cog"></i> ' .$activiteit->naam. ' vrijwilliger taken</button>').'</td>';
+            echo anchor('admin/dash/updatekeuzemogelijkheid/'.$activiteit->id,'<button class="btn btn-round btn-warning" title="Druk hier om de keuzemogelijkheid aan te passen"><i class="fa fa-cog"></i> ' .$activiteit->naam. ' aanpassen</button>').'</td>';
+            echo anchor('Keuzemogelijkheid/delete/' . $activiteit->id, '<button type="button" class="btn btn-danger btn-round" title="Druk hier om de keuzemogelijkheid te verwijderen"><i class="fa fa-trash"></i> '. $activiteit->naam . 'Verwijderen</button></div>');
 
         }
     }
@@ -53,3 +53,32 @@ if(count($data['keuzemogelijkheden']) > 0) {
         echo 'No data';   
     }?>
 </div>
+<div class="modal fade" id="keuzeoptieModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Weet u zeker dat u deze keuzeoptie wil verwijderen?</p>
+      </div>
+      <div class="modal-footer">
+        <a id="verwijderenKeuzeoptie" class="btn btn-danger btn-round">Verwijderen</a>
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Annuleer</button>
+      </div>
+    </div>
+  </div>
+</div>
+<script>
+$(document).ready(function(){
+    $('.deleteKeuzeoptie').click(function(){
+        keuzeoptieId=$(this).val();
+        $('#verwijderenKeuzeoptie').attr("href", "keuzeoptie/delete/"+keuzeoptieId);
+    });
+
+
+});
+</script>
