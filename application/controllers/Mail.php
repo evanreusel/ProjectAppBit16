b367c35695ad0c665eace29cc55a757a459c2027
Erik <evanreusel@gmail.com>
2018-05-11 16:56:25 +0200

    debugging

--- application/controllers/Mail.php
+++ application/controllers/Mail.php
@@ -143,6 +143,7 @@ class Mail extends CI_Controller {
             foreach ($keuzemogelijkheid->keuzeopties as $keuzeoptie) {
                 $keuzeoptie->personen = array();
                 $keuzeoptieVanDeelnemer = $this->KeuzeoptieVanDeelnemer_model->get_byKeuzeoptieId($keuzeoptie->id);
+                print_r($keuzeoptieVanDeelnemer);
                 $deelnemendPersoon = $this->Persoon_model->get_byId($keuzeoptieVanDeelnemer->persoonId);
                 array_push($keuzeoptie->personen, $deelnemendPersoon);
 
