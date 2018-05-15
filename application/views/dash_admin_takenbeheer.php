a008c466ec1b95ea4775a0a8b85c332875e25334
TimSwerts <de.swerts@hotmail.com>
2018-05-15 15:58:55 +0200

    +

--- application/views/dash_admin_takenbeheer.php
+++ application/views/dash_admin_takenbeheer.php
@@ -37,7 +37,7 @@ if(count($taken) > 0) {
             echo "</table>";
             echo anchor('admin/dash/update/'.$taak->id.'i',"Shift toevoegen",'class="btn btn-primary" title="Druk hier om een shift toe te voegen"');
             echo anchor('admin/dash/updatetaak/'.$taak->id.'u','<button class="btn btn-round btn-warning" title="Druk hier om de Taak aan te passen"><i class="fa fa-cog"></i> ' .$taak->functie. ' aanpassen</button>').'</td>';
-            echo anchor('Keuzemogelijkheid/delete/' . $taak->id, '<button type="button" class="btn btn-danger btn-round" title="Druk hier om de Taak te verwijderen"><i class="fa fa-trash"></i> '. $taak->functie . ' verwijderen</button></div>');
+            echo anchor('taken/delete/' . $taak->id, '<button type="button" class="btn btn-danger btn-round" title="Druk hier om de Taak te verwijderen"><i class="fa fa-trash"></i> '. $taak->functie . ' verwijderen</button></div>');
 
         }
     }
