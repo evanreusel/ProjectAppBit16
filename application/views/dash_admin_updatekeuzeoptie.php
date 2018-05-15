24609367504d39be093a2df8d0eee48d427dd77c
TimSwerts <de.swerts@hotmail.com>
2018-05-15 22:54:46 +0200

    +

--- application/views/dash_admin_updatekeuzeoptie.php
+++ application/views/dash_admin_updatekeuzeoptie.php
@@ -66,12 +66,12 @@
     </div>
     <div class="form-group row">
         <label for="minimum">Minimum aantal personen</label>
-        <?php echo form_input(array('id'=>'minimum', 'name'=>'min', 'type'='number'),$keuzeoptie->min,$nummerAttributen); ?>
+        <?php echo form_input(array('id'=>'minimum', 'name'=>'min', 'type'=>'number'),$keuzeoptie->min,$nummerAttributen); ?>
 
     </div>
     <div class="form-group row">
         <label for="maximum">Maximum aantal personen</label>
-        <?php echo form_input(array('id'=>'maximum', 'name'=>'max', 'type'='number'),$keuzeoptie->max,$nummerAttributen); ?>
+        <?php echo form_input(array('id'=>'maximum', 'name'=>'max', 'type'=>'number'),$keuzeoptie->max,$nummerAttributen); ?>
 
     </div>
     <div class="form-group row">
