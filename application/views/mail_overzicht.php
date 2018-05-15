50fda8629b0ead903c8389622314fca12ddc5f57
Erik <evanreusel@gmail.com>
2018-05-15 12:25:14 +0200

    Add Mail overzicht Select All Checkboxes

--- application/views/mail_overzicht.php
+++ application/views/mail_overzicht.php
@@ -5,36 +5,32 @@
         </div>
         <div class="card-body">
 
-            <?php foreach ($reminders as $reminder) {
-                ?>
-                <div class="card">
-                    <div class="card-header" id="reminderHoofding<?php echo $reminder->id ?>">
-                        <h5 class="mb-0">
-                            <button class="btn btn-link collapsed" data-toggle="collapse"
-                                    data-target="#reminderInhoud<?php echo $reminder->id ?>" aria-expanded="false"
-                                    aria-controls="reminderInhoud<?php echo $reminder->id ?>">
-                                <?php echo $reminder->sjabloon->naam ?>" op <?php echo $reminder->timer ?>
-                            </button>
-                        </h5>
-                    </div>
-                    <div id="reminderInhoud<?php echo $reminder->id ?>" class="collapse"
-                         aria-labelledby="reminderHoofding<?php echo $reminder->id ?>" data-parent="#accordion">
-                        <div class="card-body">
-                            <p><?php echo $reminder->timer ?></p>
-                            <p><?php echo count($reminder->ontvangers) ?> ontvangers</p>
-                            <p><?php echo $reminder->sjabloon->naam ?></p>
-                            <p><?php echo $reminder->status ?></p>
+                <?php foreach ($reminders as $reminder) {
+                    ?>
+                    <div class="card">
+                        <div class="card-header" id="reminderHoofding<?php echo $reminder->id?>">
+                            <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#reminderInhoud<?php echo $reminder->id?>" aria-expanded="false" aria-controls="reminderInhoud<?php echo $reminder->id?>">
+                                    <?php echo $reminder->sjabloon->naam ?>" op <?php echo $reminder->timer ?>
+                                </button>
+                            </h5>
                         </div>
-                        <div class="card-footer">
-                            <div class="btn-group align-content-center btn-block">
-                                <a href="#modalReminder" class="btn btn-success btn-block" data-toggle="modal"
-                                   data-target="#modalReminder">Aanpassen</a>
-                                <button class="btn btn-danger  btn-block">Verwijderen</button>
+                        <div id="reminderInhoud<?php echo $reminder->id?>" class="collapse" aria-labelledby="reminderHoofding<?php echo $reminder->id?>" data-parent="#accordion">
+                            <div class="card-body">
+                                <p><?php echo $reminder->timer ?></p>
+                                <p><?php echo count($reminder->ontvangers) ?> ontvangers</p>
+                                <p><?php echo $reminder->sjabloon->naam ?></p>
+                                <p><?php echo $reminder->status ?></p>
+                            </div>
+                            <div class="card-footer">
+                                <div class="btn-group align-content-center btn-block">
+                                    <a href="#modalReminder" class="btn btn-success btn-block" data-toggle="modal" data-target="#modalReminder">Aanpassen</a>
+                                    <button class="btn btn-danger  btn-block">Verwijderen</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            <?php } ?>
+                <?php } ?>
         </div>
     </div>
 
@@ -42,26 +38,22 @@
         <div class="card-header">Mailsjablonen</div>
         <div class="card-body">
             <div class="row row-eq-height">
-                <?php foreach ($mailsjablonen as $sjabloon) { ?>
-                    <div class="col-lg-12">
+                <?php foreach ($mailsjablonen as $sjabloon) {?>
+                <div class="col-lg-12">
 
-                        <div class="card">
-                            <div class="card-header"><?php echo $sjabloon->naam ?></div>
-                            <div class="card-body">
-                                <?php echo $sjabloon->inhoud ?>
-                            </div>
-                            <div class="card-footer">
-                                <div class="btn-group align-content-center btn-block">
-                                    <a href="#modalSjabloon" class="btn btn-success btn-block open-sjabloonvenster"
-                                       data-toggle="modal" data-target="#modalSjabloon"
-                                       data-sjabloon-naam="<?php echo $sjabloon->naam ?>"
-                                       data-sjabloon-inhoud="<?php echo $sjabloon->inhoud ?>"
-                                       data-sjabloon-id="<?php echo $sjabloon->id ?>">Aanpassen</a>
-                                    <a href="#" class="btn btn-danger  btn-block">Verwijderen</a>
-                                </div>
+                    <div class="card">
+                        <div class="card-header"><?php echo $sjabloon->naam?></div>
+                        <div class="card-body">
+                            <?php echo $sjabloon->inhoud?>
+                        </div>
+                        <div class="card-footer">
+                            <div class="btn-group align-content-center btn-block">
+                                <a href="#modalSjabloon" class="btn btn-success btn-block open-sjabloonvenster" data-toggle="modal" data-target="#modalSjabloon" data-sjabloon-naam="<?php echo $sjabloon->naam?>" data-sjabloon-inhoud="<?php echo $sjabloon->inhoud?>" data-sjabloon-id="<?php echo $sjabloon->id?>">Aanpassen</a>
+                                <a href="#" class="btn btn-danger  btn-block">Verwijderen</a>
                             </div>
                         </div>
                     </div>
+                </div>
                 <?php } ?>
             </div>
         </div>
@@ -79,75 +71,65 @@
             </div>
             <div class="modal-body">
                 <label for="modalReminderDatum">Datum:</label>
-                <input type="date" id="modalReminderDatum" value="12-05-2018">
+                <input type="date" id="modalReminderDatum">
                 <h5>Ontvangers</h5>
                 <?php
-                foreach ($keuzemogelijkheden as $keuzemogelijkheid) {
-                    if (!$keuzemogelijkheid->verbergen) { ?>
+                foreach ($keuzemogelijkheden as $keuzemogelijkheid)
+                {
+                    if (!$keuzemogelijkheid->verbergen)
+                    { ?>
                         <div class='card'>
-                            <div class='card-header'>Activiteit <?php echo $keuzemogelijkheid->naam ?>: <span
-                                        class="pull-right"><input type="checkbox"
-                                                                  data-select="activiteit"></span></div>
-                            <div class='card-body' class="activiteit">
-                                <?php
+                        <div class='card-header'>Activiteit <?php echo $keuzemogelijkheid->naam ?>: </div>
+                        <div class='card-body'>
+                        <?php
 
-                                foreach ($keuzemogelijkheid->taken as $taak) {
-                                    if (!$taak->verbergen) {
-                                        ?>
-                                        <h5>Vrijwilligers <?php echo $taak->functie ?> <input type="checkbox"
-                                                                                              data-select="taak"></h5>
-                                        <div class="taak">
-                                            <?php
-                                            foreach ($taak->shiften as $shift) {
-                                                foreach ($shift->vrijwilligers as $vrijwilliger) {
-                                                    ?>
-                                                    <label><input type="checkbox"
-                                                                  data-persoonId="<?php echo $persoon->id ?>"> <?php echo $vrijwilliger->naam ?>
-                                                    </label>
-                                                    <?php
-                                                }
-                                            } ?>
-                                        </div>
-                                        <?php
+                        foreach ($keuzemogelijkheid->taken as $taak)
+                        {
+                            if (!$taak->verbergen)
+                            {?>
+                                <h5>Vrijwilligers <?php echo $taak->functie ?></h5>
+                                <?php
+                                foreach ($taak->shiften as $shift)
+                                {
+                                    foreach ($shift->vrijwilligers as $vrijwilliger)
+                                    {?>
+                                         <label><input type="checkbox"> <?php echo $vrijwilliger->naam ?></label>
+                                    <?php
                                     }
                                 }
-                                ?>
+                            }
+                        }
+                        ?>
 
-                                <?php
-                                foreach ($keuzemogelijkheid->keuzeopties as $keuzeoptie) {
-                                    if (!$keuzeoptie->verbergen) {
-                                        ?>
-                                        <div class="keuzeoptie">
-                                        <h5>Deelnemers <?php echo $keuzeoptie->naam ?> </h5>
-                                        <?php
-                                        foreach ($keuzeoptie->personen as $persoon) {
-                                            ?>
-                                            <label><input type="checkbox"
-                                                          data-persoonId="<?php echo $persoon->id ?>"> <?php echo $persoon->naam ?>
-                                            </label>
-                                            <?php
-                                        }
-
-                                    } ?></div><?php
-                                }
-                                ?>
-                            </div>
-                        </div>
                         <?php
+                        foreach ($keuzemogelijkheid->keuzeopties as $keuzeoptie)
+                        {
+                            if (!$keuzeoptie->verbergen)
+                            {
+                                echo "<h5>Deelnemers " . $keuzeoptie->naam . "</h5>";
+                                foreach ($keuzeoptie->personen as $persoon)
+                                {?>
+                                    <label><input type="checkbox"> <?php echo $persoon->naam ?></label>
+                                <?php
+                                }
+                            }
+                        }
+                        ?>
+                        </div>
+                        </div>
+<?php
                     }
 
                 }
-                foreach ($nietingeschrevenDeelnemers as $persoon) {
-                    ?>
-                    <label><input type="checkbox"
-                                  data-persoonId="<?php echo $persoon->id ?>"> <?php echo $persoon->naam ?></label>
+                foreach ($nietingeschrevenDeelnemers as $persoon)
+                {?>
+                    <label><input type="checkbox"> <?php echo $persoon->naam ?></label>
 
                     <?php
                 }
-                foreach ($nietingeschrevenVrijwilligers as $persoon) {
-                    ?>
-                    <label><input type="checkbox"
-                                  data-persoonId="<?php echo $persoon->id ?>"> <?php echo $persoon->naam ?></label>
+                foreach ($nietingeschrevenVrijwilligers as $persoon)
+                {?>
+                    <label><input type="checkbox"> <?php echo $persoon->naam ?></label>
 
                     <?php
                 }
@@ -157,20 +139,19 @@
                 <select id="form_mailsjabloon">
                     <?php foreach ($mailsjablonen as $sjabloon) {
                         ?>
-                        <option value="<?php echo $sjabloon->id ?>">
-                            <?php echo $sjabloon->naam ?>
+                        <option value="<?php echo $sjabloon->id?>">
+                            <?php echo $sjabloon->naam?>
                         </option>
                     <?php } ?>
                 </select>
-            </div>
 
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Save changes</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
         </div>
     </div>
-</div>
 </div>
 <div class="modal" tabindex="-1" role="dialog" id="modalSjabloon">
     <div class="modal-dialog" role="document">
@@ -193,8 +174,7 @@
                     <li><i>$token</i> voor de link die de ontvanger gebruikt om naar de website te gaan;</li>
                     <li><i>$soort</i> voor de link die de ontvanger gebruikt om naar de website te gaan;</li>
                 </ul>
-                <textarea id="sjabloon-inhoud" class="form-control"
-                          placeholder="Beste $naam,&#10;Welkom! U kunt via deze link naar onze pagina gaan: $token"></textarea>
+                <textarea id="sjabloon-inhoud" class="form-control" placeholder="Beste $naam,&#10;Welkom! U kunt via deze link naar onze pagina gaan: $token"></textarea>
 
             </div>
             <div class="modal-footer">
@@ -206,12 +186,13 @@
 </div>
 <script>
 
-    $(document).ready(function () {
-        $(".open-sjabloonvenster").click(function () {
+    $( document ).ready(function() {
+        $( ".open-sjabloonvenster" ).click(function() {
             console.log("SJABLOON");
             var sjabloonId = $(this).data('sjabloon-id');
             $("#sjabloon-id").val(sjabloonId);
-            if (sjabloonId != 0) {
+            if(sjabloonId != 0)
+            {
                 // sjabloon bewerken
                 $("#sjabloon-naam").val($(this).data('sjabloon-naam'));
                 $("#sjabloon-inhoud").val($(this).data('sjabloon-inhoud'));
