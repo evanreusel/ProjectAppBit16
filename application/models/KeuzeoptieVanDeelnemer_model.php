388703c90214bce8d8585f945953aa6d5b5c134d
TimSwerts <de.swerts@hotmail.com>
2018-05-15 14:40:04 +0200

    +

--- application/models/KeuzeoptieVanDeelnemer_model.php
+++ application/models/KeuzeoptieVanDeelnemer_model.php
@@ -35,7 +35,6 @@ class KeuzeoptieVanDeelnemer_Model extends CI_Model{
     }
 
     function addKeuzeoptieVanDeelnemer($KeuzeoptieVanDeelnemer){
-        echo print_r($KeuzeoptieVanDeelnemer);
         $this->db->insert('KeuzeoptieVanDeelnemer', $KeuzeoptieVanDeelnemer);
         // return $this->db->insert_id();
     }
