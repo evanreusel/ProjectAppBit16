c237684c8f81d66a0b8fa8639a67ded458fc8780
Erik <evanreusel@gmail.com>
2018-05-16 15:31:26 +0200

    Doxygen

--- application/models/PersoonInHerinnering_model.php
+++ application/models/PersoonInHerinnering_model.php
@@ -7,23 +7,39 @@ PERSOON IN HERINNERING MODEL
 
 class PersoonInHerinnering_model extends CI_Model
 {
+    /**
+     * Default constructor
+     */
     function __construct()
     {
         $this->load->database();
     }
-
+    /**
+     * Haalt een emailherinnering op
+     * @param string $id
+     *  Id van emailherinnering
+     */
     function get_byHerinneringId($id)
     {
         $this->db->where('mailherinneringId', $id);
         $query = $this->db->get('PersoonInHerinnering');
         return $query->result();
     }
+    /**
+     * Maakt een nieuwe record aan voor persoon in herinnering
+     * @param string $id
+     *  PersoonInHerinnering object
+     */
     function insert($persoonInherinnering) {
         $this->db->insert('PersoonInHerinnering', $persoonInherinnering);
         return true;
     }
 
-
+    /**
+     * Verwijderd de persoon in een specifieke herinnering
+     * @param string $id
+     *  Id van Herinnering
+     */
     function delete($herinneringId) {
         $this->db->where('mailherinneringId', $herinneringId);
         $this->db->delete('PersoonInHerinnering');
