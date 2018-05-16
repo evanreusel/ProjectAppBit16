e2886a4362c37000b83c61b65bdbf4cf1547f351
unknown <daan proost>
2018-05-16 15:34:44 +0200

    commentaar

--- application/models/KeuzeoptieVanDeelnemer_model.php
+++ application/models/KeuzeoptieVanDeelnemer_model.php
@@ -1,6 +1,7 @@
-
-
 <?php
+// LAATST GEÜPDATET OP 16/05/18
+
+// ====================================================================== PROOST DAAN
 class KeuzeoptieVanDeelnemer_Model extends CI_Model{
     
     
@@ -9,31 +10,32 @@ class KeuzeoptieVanDeelnemer_Model extends CI_Model{
         $this->load->database();
 
     }
-
+    /// opvragen van keuzeoptie door id mee te geven
     function get_byKeuzeoptieId($id)
     {
         $this->db->where('keuzeoptieId', $id);
         $query = $this->db->get('KeuzeoptieVanDeelnemer');
         return $query->result();
     }
-    
+    /// opvragen van persoon door id mee te geven
     function get_byPersoonId($id)
     {
         $this->db->where('persoonId', $id);
         $query = $this->db->get('KeuzeoptieVanDeelnemer');
         return $query->result();
     }
-
+    /// toevoegen van nieuwe keuzeoptie van een persoon aan de database
     function add($KeuzeoptieVanDeelnemer){
         $this->db->insert('KeuzeoptieVanDeelnemer', $KeuzeoptieVanDeelnemer);
 
     }
     
-
-    function delete($shiftId, $persoonId){
+    /// Verwijderen van keuzeoptie van een persoon uit de database
+    function delete($keuzeoptieId, $persoonId){
         $this->db->where('persoonId', $persoonId);
-        $this->db->where('keuzeoptieId',$shiftId);
+        $this->db->where('keuzeoptieId',$keuzeoptieId);
         $this->db->delete('KeuzeoptieVanDeelnemer');
     }
 }
+// ====================================================================== / PROOST DAAN
 ?>
