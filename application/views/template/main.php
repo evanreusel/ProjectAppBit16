ba03252bb460c6f9e53f4da1780b6a32d97b134d
Greif Matthias <greif.matthias@outlook.com>
2018-05-16 13:13:30 +0200

    footer

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -51,22 +51,7 @@
     </div>
 
     <!--Footer-->
-<footer class="page-footer font-small blue pt-4 mt-4">
-
-<!--Footer Links-->
-<div class="container-fluid text-center text-md-left">
-    <div class="row">
-
-        <!--First column-->
-        <div class="col-md-6">
-            <h5 class="text-uppercase">Footer Content</h5>
-            <p>Here you can use rows and columns here to organize your footer content.</p>
-        </div>
-        <!--/.First column-->
-    </div>
-</div>
-<!--/.Footer Links-->
-
+<footer class="footer sticky-footer font-small blue pt-4 mt-4">
 <!--Copyright-->
 <div class="footer-copyright py-3 text-center">
     UW NAAM HIER
