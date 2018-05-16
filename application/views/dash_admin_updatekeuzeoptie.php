8ed40724898c870bcad3ffaa8db0898a75fd08f7
TimSwerts <de.swerts@hotmail.com>
2018-05-16 17:44:09 +0200

    +

--- application/views/dash_admin_updatekeuzeoptie.php
+++ application/views/dash_admin_updatekeuzeoptie.php
@@ -97,8 +97,8 @@
             $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd'});
         });
         $("#keuzeoptieForm").submit(function(event) {
-            var min = $('#minimum').val();
-            var max = $('#maximum').val();
+            var min = $('#minimum').html();
+            var max = $('#maximum').html();
             var begin =$('#begin').val(); 
             var einde =$('#einde').val();
             var naam = $('#keuzeoptie').val();
