4cbdcd07b00df58f889dbd8b9baf7422589b99be
unknown <daan proost>
2018-05-16 14:35:33 +0200

    zeg

--- application/views/dash_admin_plaatsToevoegen.php
+++ application/views/dash_admin_plaatsToevoegen.php
@@ -51,7 +51,7 @@ function ajaxplaats(plaatsId){
         <?php
         foreach ($plaatsen as $plaats) {
             echo "<tr><td>" . $plaats->naam . "</td><td>" . $plaats->locatie . "</td><td>" . '<button onclick="ajaxplaats(' . $plaats->id . ')" type="button" class="btn btn-warning btn-round wijzig" value="' . $plaats->id . '" title="Druk hier om deze locatie te wijzigen" >Wijzig</button>' . "</td><td>" .
-            '<button class="verwijder btn btn-danger btn-round" data-toggle="modal" data-target="#taakModal" value="Plaats/verwijder/"' . $plaats->id . '" title="Druk hier om deze locatie te verwijderen">Verwijderen</button></td></tr>';
+            '<button class="verwijder btn btn-danger btn-round" data-toggle="modal" data-target="#taakModal" value="Plaats/verwijder/' . $plaats->id . '" title="Druk hier om deze locatie te verwijderen">Verwijderen</button></td></tr>';
         }
         
         ?>
@@ -109,6 +109,6 @@ if(isset($huidigePlaats)){$plaatsTest = $huidigePlaats->naam; $locatieTest = $hu
 </div>
 <script>
 $('.verwijder').Click(function(){
-    $('#verwijderenLocatie').attr("href",$(this).val()):
+    $('#verwijderenLocatie').attr("href",$(this).val());
 });
 </script>
